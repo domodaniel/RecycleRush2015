d2af4df6d45792ee25b269b744fc159d8326d349
Wesley Soo-Hoo <wsoohoo32@gmail.com>
2015-02-06 21:38:09 -0800

    Start Articulation

--- src/org/usfirst/frc/team687/articulation/NerdyArtic.java
+++ src/org/usfirst/frc/team687/articulation/NerdyArtic.java
@@ -1,5 +1,29 @@
 package org.usfirst.frc.team687.articulation;
 
 public class NerdyArtic {
-
+	private static double power;
+	private static double height;
+	private static double level;
+	
+	public static void setHeight(double h)	{
+		height = h;
+		NerdyArticPID.setHeight(height);
+	}
+	
+	public static double getHeight()	{
+		return height;
+	}
+	
+	public static void set(int l)	{
+		NerdyArticPID.pid(l);
+		level = l;
+	}
+	
+	public static double getLevel()	{
+		return level;
+	}
+	
+	public static double getPower()	{
+		return NerdyArticPID.getPower();
+	}
 }
--- src/org/usfirst/frc/team687/articulation/NerdyArticPID.java
+++ src/org/usfirst/frc/team687/articulation/NerdyArticPID.java
@@ -1,5 +1,38 @@
 package org.usfirst.frc.team687.articulation;
 
-public class NerdyArticPID {
+import org.usfirst.frc.team687.util.NerdyIntegrator;
 
+public class NerdyArticPID {
+	private static NerdyIntegrator integrator;
+	private static boolean ini = false;
+	private final static double kP = 0.1, kI = 0.01;
+	private final static double inches = 5;
+	private static double height = 0;
+	private static double power = 0;
+	
+	public static void init()	{
+		integrator = new NerdyIntegrator(kI);
+		ini = true;
+	}
+	
+	public static void setHeight(double h)	{
+		height = h;
+	}
+	
+	public static void pid(int level)	{
+		if(!ini)	{
+			init();
+		}
+		double desired = level * inches;
+		double error = desired-height;
+		double p = kP * error;
+		integrator.setError(error);
+		integrator.perform();
+		double i = integrator.getI();
+		power = (p+i);
+	}
+	
+	public static double getPower()	{
+		return power;
+	}
 }
